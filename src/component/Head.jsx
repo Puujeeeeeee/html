17d5cfa2e5989f2da9f55d543704a1c44119d6d6
Purevsambuu Bukhbayar <23LP4405@imac888.local>
2024-01-17 19:58:52 -0800

    porfolio

---
+++ src/component/Head.jsx
@@ -0,0 +1,14 @@
+function Head() {
+  return (
+    <div>
+      <div>
+        <p class="text-sm">about</p>
+        <p></p>
+        <p></p>
+        <p></p>
+      </div>
+    </div>
+  );
+}
+
+export default Head;
---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import Head from "../component/Head";
+export default function Home() {
+  return (
+    <div className="flex justify-center bg-red-900 ">
+      <div>
+        <Head></Head>
+      </div>
+    </div>
+  );
+}
